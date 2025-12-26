16973027c118bfff7978db23bbe5e33b29b3dcfb
reliquiaberg <queroxt66@gmail.com>
2025-12-26 19:48:47 -0300

    bot com musica

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus
+} = require('@discordjs/voice');
+const play = require('play-dl');
 
 const client = new Client({
   intents: [
@@ -10,27 +16,52 @@ const client = new Client({
   ]
 });
 
+const player = createAudioPlayer();
+
 client.once('ready', () => {
   console.log('Bot ligado!');
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
 
+  // ENTRAR NA CALL
   if (message.content === '!entrar') {
     if (!message.member.voice.channel) {
-      message.reply('Entre em uma call primeiro');
-      return;
+      return message.reply('Entre em uma call primeiro');
     }
 
-    joinVoiceChannel({
+    const connection = joinVoiceChannel({
       channelId: message.member.voice.channel.id,
       guildId: message.guild.id,
       adapterCreator: message.guild.voiceAdapterCreator
     });
 
+    connection.subscribe(player);
     message.reply('Entrei na call');
   }
+
+  // TOCAR MÚSICA
+  if (message.content.startsWith('!play')) {
+    const args = message.content.split(' ');
+    const url = args[1];
+
+    if (!url) {
+      return message.reply('Coloque o link da música');
+    }
+
+    if (!message.member.voice.channel) {
+      return message.reply('Entre em uma call primeiro');
+    }
+
+    const stream = await play.stream(url);
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type
+    });
+
+    player.play(resource);
+    message.reply('🎶 Tocando música');
+  }
 });
-console.log("TOKEN EXISTE?", !!process.env.TOKEN);
+
 client.login(process.env.TOKEN);
